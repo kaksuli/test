359b1e098ae1908ae342dc0275ab68a75cb14226
sulaiman kadkhodaei <mpsk7492@gmail.com>
2020-07-28 13:21:53 +0430

    add Q11

--- src/main/java/FeedSchedule.java
+++ src/main/java/FeedSchedule.java
@@ -1,6 +1,6 @@
 public class FeedSchedule {
     public static void main(String[] args) {
-        boolean keepGoing = true;
+        /*boolean keepGoing = true;
         int count = 0;
         int x = 3;
         while (count++ < 3){
@@ -10,7 +10,23 @@ public class FeedSchedule {
                 case 0: x -= 1; break;
                 case 1: x += 5;
             }
+        }*/
+
+        int x = 5, j = 0;
+        OUTER:
+        for (int i = 0; i < 3; ) {
+            INNER:
+            do {
+                i++;
+                x++;
+                if (x > 10) {
+                    break INNER;
+                }
+                x += 4;
+                j++;
+            } while (j <= 2);
         }
+
         System.out.println(x);
     }
 }
